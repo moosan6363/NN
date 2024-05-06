1487886dc4d4931c371854dd5c454197fd4d2aca
nomura <moosan6363@gmail.com>
2024-05-07 02:56:49 +0900

    refactor: Update neural network architecture and data visualization
    - Update the input dimension of the first linear layer in the `Network` class from 1 to 2 to match the input data shape.
    - Generate random input data with shape (1000, 2) instead of using a predefined range.
    - Modify the ground truth data generation formula to `(1 + np.sin(4 * np.pi * x[:, 0])) * x[:, 1] / 2`.
    - Adjust the loss calculation to use the updated ground truth data.
    - Update the scatter plot visualization to include the ground truth data and the predicted data in a 3D plot.
    
    The changes improve the neural network's ability to learn and predict the target variable based on the updated input data. The visualization also provides a clearer representation of the relationship between the input variables and the target variable.

--- exp/demo.py
+++ exp/demo.py
@@ -6,7 +6,7 @@ import matplotlib.pyplot as plt
 class Network(nn.Network):
     def __init__(self):
         super().__init__()
-        self.add(nn.Linear(1, 16))
+        self.add(nn.Linear(2, 16))
         self.add(nn.ReLU())
         self.add(nn.Linear(16, 16))
         self.add(nn.ReLU())
@@ -18,23 +18,29 @@ if __name__ == "__main__":
 
     network = Network()
     criterion = nn.MSE(network.backward)
-    # optimizer = nn.SGD(network.layers, lr=0.01, weight_decay=0.0, momentum=0.001, nesterov=True)
+    # optimizer = nn.SGD(
+    #     network.layers, lr=0.01, weight_decay=0.0, momentum=0.1, nesterov=True
+    # )
     optimizer = nn.Adam(network.layers, lr=0.01)
 
-    x = np.linspace(-1, 1, 100).reshape(-1, 1)
-    y = x**3
+    x = np.random.rand(1000, 2)
+    z = (1 + np.sin(4 * np.pi * x[:, 0])) * x[:, 1] / 2
+    z = np.expand_dims(z, axis=1)
 
     for epoch in range(20000):
         optimizer.zero_grad()
         pred = network(x)
-        loss = criterion(pred, y)
+        loss = criterion(pred, z)
         criterion.backward()
         optimizer.step()
         if epoch % 100 == 0:
             print(f"epoch: {epoch}, loss: {loss}")
 
-    pred = network(x)
-    plt.plot(x, y, label="true")
-    plt.plot(x, pred, label="pred")
-    plt.legend()
-    plt.savefig("result.png")
+    z_pred = network(x)
+    fig, ax = plt.subplots(figsize=(6, 6), subplot_kw={"projection": "3d"})
+    ax.scatter(x[:, 0], x[:, 1], z, label="ground_truth", color="red")
+    ax.scatter(x[:, 0], x[:, 1], z_pred, label="prediction", color="blue")
+    ax.set_xlabel("x_1")
+    ax.set_ylabel("x_2")
+    ax.set_zlabel("z")
+    fig.savefig("result.png")
